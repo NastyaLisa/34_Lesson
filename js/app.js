ce18eaeb2ec7aaa54d7fefc0cdaab943f5f92f21
NastyaLisa <lis.nastiona@gmail.com>
2023-02-26 22:33:59 +0200

    done

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//Task 1
+// import  {employees} from "./employees.js";
+// import {totalSalary} from "./methods.js";
+// import {totalSalary2} from "./methods.js";
+
+// console.log('Task 1',totalSalary(employees));
+// console.log('Task 1',totalSalary2(employees));
+
+// Task 2
+// import { departmentsQauntity } from "./methods.js";
+// console.log('Task 2', departmentsQauntity(employees));
+
+
+//Task 3
+// import { departmentsSalary } from "./methods.js";
+// console.log('Task 3',departmentsSalary(employees));
+
+// 3.1
+import  employees from "./employees.js";
+
+// 3.2
+import * as employeesMethods from "./methods.js";
+console.log(employeesMethods.departmentsSalary(employees));
